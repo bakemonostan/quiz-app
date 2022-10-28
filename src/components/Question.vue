986eaf9f7869aa4383b4fe002406653e73846e0c
bakemonostan <ehizomokhomion@gmail.com>
2022-10-28 13:39:10 -0700

    set up question component and get questions dynamically

---
+++ src/components/Question.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="question-container">
+      <h5 class="question">{{ question.text }}</h5>
+    </div>
+    <div class="options-container">
+      <div class="option">
+        <p class="option-label">A</p>
+        <div class="option-value">
+          <p>NaCL</p>
+        </div>
+      </div>
+      <div class="option">
+        <p class="option-label">A</p>
+        <div class="option-value">
+          <p>NaCL</p>
+        </div>
+      </div>
+      <div class="option">
+        <p class="option-label">A</p>
+        <div class="option-value">
+          <p>NaCL</p>
+        </div>
+      </div>
+      <div class="option">
+        <p class="option-label">A</p>
+        <div class="option-value">
+          <p>NaCL</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const { question } = defineProps(['question']);
+</script>
+
+<style scoped>
+.question-container {
+  margin-top: 20px;
+}
+
+.question {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+.option {
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.option-label {
+  background-color: bisque;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.option-value {
+  width: 100%;
+  padding: 0 20px;
+  background-color: rgb(244, 239, 239);
+  font-size: 30px;
+}
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,9 +1,51 @@
 <template>
   <div>
-    <h3>Welcome to quiz page</h3>
+    <header>
+      <h4>Question 1/3</h4>
+      <div class="bar">
+        <div class="completion"></div>
+      </div>
+    </header>
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
   </div>
 </template>
 
+<script setup>
+import Question from '../components/Question.vue';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+import quizes from '../data/data.json';
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+
+// states
+const currentQuestionIndex = ref(0);
+const quiz = quizes.find((q) => q.id === quizId);
+</script>
+
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+header {
+  margin-top: 20px;
+}
+
+header h4 {
+  font-size: 30px;
+}
+
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid bisque;
+}
+
+.completion {
+  height: 100%;
+  width: 0%;
+  background-color: bisque;
+}
+</style>
