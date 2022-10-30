29df15759d9ef8181a96cad8ab496c3675085d91
bakemonostan <ehizomokhomion@gmail.com>
2022-10-29 18:07:42 -0700

    go back btn

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,11 +2,17 @@
   <div class="result">
     <p>Your Result</p>
     <h4>Your score is {{ correctAnswers }}</h4>
+    <RouterLink to="/">
+      <button>Go home</button>
+    </RouterLink>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+
+// states
 const { correctAnswers } = defineProps(['correctAnswers']);
 </script>
 
